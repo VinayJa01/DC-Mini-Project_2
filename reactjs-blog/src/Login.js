f3140c2be99794160c27c2eff1817770575c5edd
Vinay <vinaypjadhav952@gmail.com>
2024-02-19 13:19:52 +0530

    new commit

--- reactjs-blog/src/Login.js
+++ reactjs-blog/src/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css";
+import axios from "axios";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -19,26 +20,57 @@ const Login = () => {
     setLoading(true);
 
     try {
-      console.log(formData)
-      const response = await fetch("http://127.0.0.1:8000/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      // console.log(formData)
+      // const response = await fetch("http://127.0.0.1:8000/login", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(formData),
+      //});
 
-      if (response.ok) {
-        const { token } = await response.json();
 
-        // Store the token in local storage
-        localStorage.setItem("jwtToken", token);
-        console.log
-        console.log("User logged in successfully!");
-      } else {
-        const errorData = await response.json();
-        setError(errorData.message);
-      }
+      // if (response.ok) {
+      //   const { token } = await response.json();
+
+      //   // Store the token in local storage
+      //   localStorage.setItem("token", token.access);
+      //   console.log(token);
+      //   console.log("User logged in successfully!");
+      // } else {
+      //   const errorData = await response.json();
+      //   setError(errorData.message);
+      // }
+
+      //await axios.post("http://127.0.0.1:8000/login/", formData).then(data => console.log(data.data))
+     
+      const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify(formData)
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+console.log(formData);
+console.log(raw);
+
+fetch("http://127.0.0.1:8000/login/", requestOptions)
+  .then((response) => response.json())
+  .then((result) => {console.log(result)
+   localStorage.setItem("token", result.token.access)
+   localStorage.getItem("token")
+   let x = localStorage.getItem("token")
+   console.log("token recieved", x);
+  })
+  .catch((error) => console.error(error));
+
+
+
     } catch (error) {
       console.error("Error during login:", error);
       setError("An unexpected error occurred");
